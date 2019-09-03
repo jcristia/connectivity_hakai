6d38d86467888b59d14a21b048cfb883a0daed77
John <jcristia10@gmail.com>
2019-09-03 10:55:08 -0700

    combine u and v and crop nemo model

--- scripts_dev_scratch/hydro_models_format/hydro_models_format_nemo.py
+++ scripts_dev_scratch/hydro_models_format/hydro_models_format_nemo.py
@@ -1,1 +1,116 @@
+import netCDF4 as nc
+import os
+import numpy as np
+import subprocess
 
+
+#######
+# Explore
+#######
+
+filename = r'D:/Hakai/NEP36-N26_5d_20140524_20161228_grid_U_20160712-20160721.nc'
+
+dataset = nc.Dataset(filename, "r+")
+variables = dataset.variables.keys()
+for variable in variables:
+    print (variable)
+for dim in dataset.dimensions.values():
+    print (dim)
+for var in dataset.variables.values():
+    print (var)
+
+time = dataset.variables["time_counter"][:]
+depth = dataset.variables["depthu"][:] # this seems to be the center of a slice
+depth_b = dataset.variables["depthu_bounds"][:] # these are the bounds of that slice
+lon = dataset.variables["nav_lon"]
+lat = dataset.variables["nav_lat"]
+#lon[0] # notice how there are zeros at the end of every slice. This screws up how it gets read in by the reader. I need to crop these out.
+
+##############################
+
+
+#######
+# put U and V variables into one file
+#######
+
+import subprocess
+import os
+import netCDF4 as nc
+path = r'D:/Hakai'
+os.chdir(path)
+os.getcwd()
+u_file_copy = r'NEP36-N26_5d_20140524_20161228_grid_UV_20160712-20160721_COMBINED.nc' # this can just be a renamed copy of the original U file
+v_file = r'NEP36-N26_5d_20140524_20161228_grid_V_20160712-20160721.nc'
+command = '''ncks -A -v vo ''' + v_file + " " + u_file_copy
+subprocess.call(command)
+# check
+dataset = nc.Dataset(u_file_copy, "r+")
+for var in dataset.variables.values():
+    print (var)
+uo = dataset.variables["uo"]
+vo = dataset.variables["vo"]
+
+###########################################
+
+
+
+#######
+# cropping a netCDF DEPTH
+#######
+
+# this works but you can't run it repeadetly for some reason. Had trouble overwriting itself.
+
+import subprocess
+import os
+import netCDF4 as nc
+path = r'D:/Hakai'
+os.chdir(path)
+os.getcwd()
+infile = r'NEP36-N26_5d_20140524_20161228_grid_UV_20160712-20160721_COMBINED.nc'
+outfile = r'NEP36-N26_5d_20140524_20161228_grid_UV_20160712-20160721_COMBINED_VCROPPED.nc'
+command = '''ncks -d depthu,1,1 -d depthv,1,1 ''' + infile + " -O " + outfile
+subprocess.call(command)
+# check
+dataset = nc.Dataset(outfile, "r+")
+for dim in dataset.dimensions.values():
+    print (dim)
+for var in dataset.variables.values():
+    print (var)
+
+###########################################
+
+
+
+
+#########
+# cropping a netcdf X/Y
+# INCOMPLETE
+#########
+
+# https://stackoverflow.com/questions/25731393/is-there-a-way-to-crop-a-netcdf-file
+
+#outname = r'D:/Hakai/NEP36-N26_5d_20140524_20161228_grid_U_20160712-20160721_CROPPED.nc'
+## find max of lat and lon values through all of data. Mask out the 0 values.
+#ma_y = np.ma.masked_equal(lat, 0.0, copy=False)
+#ymax = np.amax(ma_y)
+#ymin = np.amin(ma_y)
+#ma_x = np.ma.masked_equal(lon, 0.0, copy=False)
+#xmax = np.amax(ma_x)
+#xmin = np.amin(ma_x)
+# this ended up being useless since I can only crop by x and y dimensions which is just a generic grid
+
+#path = r'D:/Hakai'
+#os.chdir(path)
+#os.getcwd()
+#command = '''ncks -d x,250,714 -d y,250,750 NEP36-N26_5d_20140524_20161228_grid_U_20160712-20160721.nc -O cropped_test.nc'''
+## -d is dimension
+## -O allows for overwrite of existing output file
+#subprocess.call(command)
+
+# this works, but all the 0 values are on the land side
+# I think it was an issue in how Pramod originally clipped it
+# Ideally all the 0 values could still be coordinates and they could just have 0 as their u/v values
+# OR the 0 values could be considered missing values and maybe opendrift won't read them
+# for now, I will just move on and see if it matters to opendrift.
+
+################################
--- scripts_dev_scratch/hydro_models_format/opendrift_testing.py
+++ scripts_dev_scratch/hydro_models_format/opendrift_testing.py
@@ -1,39 +1,47 @@
 import netCDF4 as nc
 
-filename = r'D:\Hakai\NEP36-N26_5d_20140524_20161228_grid_U_20160712-20160721.nc'
-#filename = r'D:\Hakai\fvcom_results\calvert03_tides_verified_EDITED.nc'
-#filename = r'C:\Users\jcristia\Downloads\opendrift_test.tar\opendrift_test\SalishSea_1h_20171110_20171110_opendrift.nc'
-#filename = r'C:\Miniconda3\envs\opendrift_p2\Lib\site-packages\opendrift-master\tests\test_data\2Feb2016_Nordic_sigma_3d\Nordic-4km_SLEVELS_avg_00_subset2Feb2016.nc'
-dataset = nc.Dataset(filename, "r+")
-print dataset
-variables = dataset.variables.keys()
-for variable in variables:
-    print variable
-for dim in dataset.dimensions.values():
-    print dim
-for var in dataset.variables.values():
-    print var
-
-time = dataset.variables["time_counter"][:]
-depth = dataset.variables["depthu"][:] # this seems to be the center of a slice
-depth_b = dataset.variables["depthu_bounds"][:] # these are the bounds of that slice
-lon = dataset.variables["nav_lon"]
-lat = dataset.variables["nav_lat"]
-lon[0] # notice how there are zeros at the end of every slice
+filename = r'D:\Hakai\NEP36-N26_5d_20140524_20161228_grid_UV_20160712-20160721_COMBINED_VCROPPED.nc'
 
 from opendrift.models.oceandrift import OceanDrift
 o = OceanDrift(loglevel=20)
 
-from opendrift.readers import reader_ROMS_native
-reader_roms = reader_ROMS_native.Reader(r'C:\Miniconda3\envs\opendrift_p2\Lib\site-packages\opendrift-master\tests\test_data\2Feb2016_Nordic_sigma_3d\Nordic-4km_SLEVELS_avg_00_subset2Feb2016.nc')
-print reader_roms
+#from opendrift.readers import reader_netCDF_CF_unstructured
+#reader_hakai = reader_netCDF_CF_unstructured.Reader(r'D:\Hakai\fvcom_results\cal03brcl_21_0003_EDITED_SHORTENED1.nc')
+#print (reader_hakai)
 
-from opendrift.readers import reader_netCDF_CF_unstructured
-reader_hakai = reader_netCDF_CF_unstructured.Reader(r'D:\Hakai\fvcom_results\calvert03_tides_verified_EDITED.nc')
-print reader_hakai
-
+# test new reader
 from opendrift.readers import reader_NEMO_pacific_JC
-reader_nemo_pacific_4 = reader_NEMO_pacific_JC.Reader(r'D:\Hakai\NEP36-N26_5d_20140524_20161228_grid_U_20160712-20160721.nc')
-print reader_nemo_pacific_4
+reader_nemo_pac = reader_NEMO_pacific_JC.Reader(filename)
+print(reader_nemo_pac)
 
+OceanDrift.fallback_values['ocean_vertical_diffusivity'] = 0.0
+OceanDrift.fallback_values['vertical_advection'] = False
 
+from opendrift.readers import reader_basemap_landmask
+
+# test with just area around Hakai for now
+reader_basemap = reader_basemap_landmask.Reader(
+                       llcrnrlon=-129.4, llcrnrlat=50.64,
+                       urcrnrlon=-126.6, urcrnrlat=52.93,
+                       resolution='f', projection='merc')
+#reader_basemap.plot()
+o.add_reader([reader_basemap, reader_nemo_pac])
+
+o.seed_elements(lon=-128.5, lat=51.5, number=100, time=reader_nemo_pac.start_time)
+o.elements_scheduled
+
+o.set_config('drift:current_uncertainty_uniform', 1)
+o.set_config('general:coastline_action', 'stranding')
+o.set_config('drift:scheme', 'euler')
+
+#o.list_configspec()
+
+from datetime import datetime
+from datetime import timedelta
+o.run(end_time=reader_nemo_pac.end_time, time_step=30, time_step_output=1800, outfile=r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\scripts_dev_scratch\hydro_models_format\output_1.nc', export_variables=["age_seconds", "land_binary_mask"])
+
+print(o)
+o.plot()
+o.animation()
+
+#####################
