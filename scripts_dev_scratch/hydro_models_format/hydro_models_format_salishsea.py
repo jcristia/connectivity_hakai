347e240dea7211a90cd9245deeaa8fe7ba19f16b
John <jcristia10@gmail.com>
2020-03-01 22:57:45 -0800

    edit to handle files with uv data in one file

--- scripts_dev_scratch/hydro_models_format/hydro_models_format_salishsea.py
+++ scripts_dev_scratch/hydro_models_format/hydro_models_format_salishsea.py
@@ -1,21 +1,26 @@
+# To run, check the 'local_add' path, and change the file name in the load loop, the date range, and the filename in the time loop.
+# This is set up to read from nc files with u/v info stored in 1 file
+# If I need to use it for u/v stored in separate files (as they would be if you download them directly) then change the file name in the load loop (an example is commented out), and change the filename in the time loop (an example is commented out)
+# ALSO, the slicing now changes since there is no depth dimension. I have commmented out areas where this has changed.
+
 # kindly borrowed from Ben Moore Maley
 # https://nbviewer.jupyter.org/urls/bitbucket.org/salishsea/analysis-ben/raw/2e76930808c2db860a25b56870859ab254f2c306/notebooks/OpenDrift/sample_opendrift_simulation.ipynb
+
+# I changed the order of some code and added in some "del" statements to remove objects from memory. Otherwise I was getting memory errors.
+# 2.5 months of data seems to be the max that my computer can handle.
 
 import numpy as np
 import xarray as xr
 import os
-from datetime import datetime, timedelta
 from dateutil.parser import parse
 
-# Define paths, grid, and mask
 paths = {
-    'erddap': 'https://salishsea.eos.ubc.ca/erddap/griddap',
     'local': r'D:\Hakai\models\salishsea',
-    'local_add': r'salishseacast_20170101_20170316'
+    'local_add': r'salishseacast'
 }
-grid = xr.open_dataset(os.path.join(paths['local'], 'ubcSSnBathymetryV17-02.nc'))
-mask = xr.open_dataset(os.path.join(paths['local'], 'ubcSSn3DMeshMaskV17-02.nc'))
 
+filename = 'ssc_20170801_20171014.nc'
+dates = ['2017 Aug 01 00:30', '2017 Oct 14 23:30']
 
 def unstagger(u, v):
     """Unstagger velocities from u,v points to t points
@@ -36,24 +41,30 @@ def rotate(u, v):
 
     return u, v
 
+# Load forcing data from ERDDAP
+raw = []
+for vel in ['u', 'v']:
+    #with xr.open_dataset(os.path.join(paths['local'], paths['local_add'], f'ubcSSg3D{vel}.nc')) as data:
+    with xr.open_dataset(os.path.join(paths['local'], paths['local_add'], filename)) as data:
+        #raw.append(data[f'{vel}Velocity'][:, 0, ...].values) #use this one if you have a depth dimension
+        raw.append(data[f'{vel}Velocity'][:, ...].values)
+
+# Unstagger velocities to T points and rotate to lon/lat
+u, v = rotate(*unstagger(*raw))
+del raw
 
 # Daterange for simulation
-daterange = [parse(d) for d in ['2017 Jan 01 00:30', '2017 Jan 05 23:30']]
+daterange = [parse(d) for d in dates]
+with xr.open_dataset(os.path.join(paths['local'], paths['local_add'], filename)) as data:
+#with xr.open_dataset(os.path.join(paths['local'], paths['local_add'], 'ubcSSg3Du.nc')) as data:
+    time = data.time.sel(time=slice(*daterange))
 
 # Forcing path
 fn = 'SalishSea_1h_' + '_'.join(d.strftime('%Y%m%d') for d in daterange) + '_opendrift.nc'
 forcing_NEMO = os.path.join(paths['local'], paths['local_add'], 'forcing', fn)
 
-
-# Load forcing data from ERDDAP
-raw = []
-for vel in ['u', 'v']:
-    with xr.open_dataset(os.path.join(paths['local'], paths['local_add'], f'ubcSS_V19-05_{vel}.nc')) as data:
-        time = data.time.sel(time=slice(*daterange))
-        raw.append(data[f'{vel}Velocity'][:, 0, ...].values)
-
-# Unstagger velocities to T points and rotate to lon/lat
-u, v = rotate(*unstagger(*raw))
+grid = xr.open_dataset(os.path.join(paths['local'], 'ubcSSnBathymetryV17-02.nc'))
+mask = xr.open_dataset(os.path.join(paths['local'], 'ubcSSn3DMeshMaskV17-02.nc'))
 
 # Reshape, remove landpoints, and save to local netCDF path
 tmask = mask.tmask[0, 0, 1:, 1:].values.reshape(-1).astype(bool)
