9ae563f3c6b6f251f1712a64bc19af7ba5ccdf61
jcristiani <jcristia10@gmail.com>
2020-07-15 15:40:05 -0700

    concatenate netcdf files

--- scripts_dev_scratch/hydro_models_format/hydro_models_format_nepnemo.py
+++ scripts_dev_scratch/hydro_models_format/hydro_models_format_nepnemo.py
@@ -8,8 +8,8 @@ import subprocess
 # Explore
 #######
 
-filename = r'D:\Hakai\models\nep_nemo\Li_sample\NEP36-OPM221_1h_20070101_20111205_grid_U_2D_20101231-20110109.nc'
-filename = r'D:\Hakai\models\nep_nemo\Li_sample\NEP36-OPM221_1h_20070101_20111205_grid_V_2D_20101231-20110109.nc'
+filename = r"C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\spatial\models\nep_nemo\Li_sample\combined\NEP36-OPM221_1h_20070101_20111205_grid_2D_20101231-20110109_UV.nc"
+filename = r"C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\spatial\models\salishsea\salishseacast\ssc_20110101_20110316.nc"
 
 dataset = nc.Dataset(filename, "r+")
 variables = dataset.variables.keys()
@@ -39,11 +39,11 @@ lat = dataset.variables["nav_lat"]
 import subprocess
 import os
 import netCDF4 as nc
-path = r'D:/Hakai/models/nep_nemo/Li_sample'
+path = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\spatial\models\nep_nemo\Li_sample'
 os.chdir(path)
 os.getcwd()
-u_file_copy = r'NEP36-OPM221_1h_20070101_20111205_grid_UV_2D_20101231-20110109_COMBINED.nc' # this can just be a renamed copy of the original U file
-v_file = r'NEP36-OPM221_1h_20070101_20111205_grid_V_2D_20101231-20110109.nc'
+u_file_copy = r'NEP36-OPM221_1h_20070101_20111205_grid_2D_20110311-20110320_UV.nc' # this can just be a renamed copy of the original U file
+v_file = r'NEP36-OPM221_1h_20070101_20111205_grid_V_2D_20110311-20110320.nc'
 command = '''ncks -A -v vos ''' + v_file + " " + u_file_copy
 subprocess.call(command)
 # check
@@ -54,6 +54,57 @@ uos = dataset.variables["uos"]
 vos = dataset.variables["vos"]
 
 ###########################################
+
+
+#######
+# concatenate all those files
+#######
+
+import os
+import sys
+# save location
+folder = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\spatial\models\nep_nemo\Li_sample\combined'
+outname = 'NEP36-OPM221_1h_20070101_20111205_grid_2D_20101231-20110320_UVALL.nc'
+# create list of files
+ncfiles = []
+#ncfiles_out = []
+for file in os.listdir(folder):
+    outfile = os.path.join(folder, file)
+    ncfiles.append(outfile)
+    # outfile = os.path.join(folder, 't' + file)
+    # ncfiles_out.append(outfile)
+   
+# NCO notes:
+#For some reason the tool "ncrcat" doesn't ship with nco for windows. However, apparently it is exactly the same thing as "ncra". Therefore, just copy ncra.exe and rename to ncrcat.exe.
+#To concatenate, you need a 'record' dimension to join on. This means a dimension that can grow to unlimited size, which is usually Time.
+#Therefore, I need to change time to a record dimension in each nc file. (DONT need to do this for NEP files. I would for Salish Sea though, so I'll leave this in here for reference)
+#I can use 'ncecat', but apparently this is slower because it automatically creates a new time dimension and then you are left with an extra dimension.
+
+# change time to a record dimension
+# for file, file_out in zip(ncfiles, ncfiles_out):
+#     command = '''ncks -O --mk_rec_dmn time {} {}'''.format(file, file_out)
+#     os.system(command)
+# nco merge
+command = '''ncrcat {} {}'''.format(' '.join(ncfiles), os.path.join(folder, outname))
+os.system(command)
+# test
+import netCDF4 as nc
+data = os.path.join(folder, outname)
+dataset = nc.Dataset(data, "r+")
+variables = dataset.variables.keys()
+for variable in variables:
+    print (variable)
+for dim in dataset.dimensions.values():
+    print (dim)
+for var in dataset.variables.values():
+    print (var)
+
+##############################################
+
+
+
+
+
 
 
 
