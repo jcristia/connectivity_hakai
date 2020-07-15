a375f7ce3e633bd5cc16d4edd4a1b20fa8f458b8
jcristiani <jcristia10@gmail.com>
2020-07-15 15:39:45 -0700

    new figure

--- scripts_dev_scratch/network_analysis/07_analysis_plotting_CONEFOR.py
+++ scripts_dev_scratch/network_analysis/07_analysis_plotting_CONEFOR.py
@@ -228,3 +228,155 @@ ax2.set_xlabel('PLD')
 ax.set_ylabel('% PCnum')
 #plt.show()
 fig.savefig(os.path.join(root, dir, output_ind, 'plds_PCintrainter_201701.svg'))
+
+
+
+
+###############################################
+# PCnum vs. averaged PLD
+# find max AL2 of these scenarios to compare to - perhaps just put as a text box
+# plot individual points and put a regression line through, x axis will be categorical?
+
+dirs = [
+    'seagrass_20200228_SS201701',
+    'seagrass_20200309_SS201705',
+    'seagrass_20200309_SS201708',
+    'seagrass_20200310_SS201101',
+    'seagrass_20200310_SS201105',
+    'seagrass_20200310_SS201108',
+    'seagrass_20200327_SS201401',
+    'seagrass_20200327_SS201405',
+    'seagrass_20200327_SS201408',
+    ]
+
+root = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\scripts_runs_cluster\seagrass'
+plds = ['01', '03', '07', '21', '60']
+overall_indices = r'conefor\conefor_connectivity_pld{}\overall_indices.txt'
+
+df_indices = pd.DataFrame()
+for pld in plds:
+    for dir in dirs:
+        indices = os.path.join(root, dir, overall_indices.format(pld))
+        df_ind = pd.read_csv(indices, sep='\t', names=['conefor_index', 'value'])
+        df_ind['pld'] = int(pld)
+        df_ind = df_ind.pivot(index='pld', columns='conefor_index', values='value').reset_index()
+        df_ind['datepld'] = dir[20:] + pld
+        df_indices = df_indices.append(df_ind)
+
+################## THIS IS THE ONE
+df_indlog = df_indices
+df_indlog['pldlog'] = np.log(df_indices.pld) # log pld
+df_indlog['season'] = df_indlog.datepld.str[4:6].astype(int) # color by season
+df_indlog['season'] = df_indlog.season.replace([1,5,8],['winter', 'spring', 'summer'])
+sns.set()
+sns.set_style('white')
+sns.set_context('paper')
+colors = ['#377eb8', '#4daf4a', '#ff7f00']
+sns.set_palette(colors)
+# I'm not going to present this as a regression. Instead I will just present the points, then draw a line connecting the means.
+# would make more sense to use scatterplot than lmplot, but scatterplot doesn't have jitter
+#sns.scatterplot(x='pldlog', y='PCnum', data=df_indlog, hue='season', legend="full", x_jitter=0.01)
+f = sns.lmplot(x="pldlog", y="PCnum", data=df_indlog, fit_reg=False, hue='season', x_jitter=0.02, legend='full')
+f._legend.remove()
+sns.lineplot(x='pldlog', y='PCnum', data=df_indlog, ci=95, err_style='band', color='grey')
+# band is a 95% confidence interval for the means
+f.set(xlabel='ln PLD')
+sns.despine(top=False, right=False)
+f.savefig(r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\publications_figures\chap1\fig04_PCnumLOG.svg')
+# to maybe do in the future:
+# draw the lineplot behind the points. However, lmplot doesn't use 'ax', so I can't figure out how to do this.
+# instead of transforming the data, just plot on a custom ln axis
+# https://matplotlib.org/3.1.0/api/_as_gen/matplotlib.axes.Axes.set_xscale.html#matplotlib.axes.Axes.set_xscale
+# https://stackoverflow.com/questions/43463431/custom-logarithmic-axis-scaling-in-matplotlib
+########################
+
+
+
+############# TESTING
+sns.set()
+sns.set_style('white')
+sns.set_context('notebook')
+# x as discrete point plot
+sns.pointplot(x='pld', y='PCnum', data=df_indices)
+# x as continuous, lineplot
+sns.lineplot(x='pld', y='PCnum', data=df_indices, err_style="bars", ci=95, marker="o")
+
+# regression
+# show means and confidence intervals
+fig, ax = plt.subplots(figsize=(13,10))
+sreg2 = sns.regplot(x="pld", y="PCnum", data=df_indices, x_estimator=np.mean, ci=95, order=2, line_kws={'linestyle': '--'})
+ax.set(xlim=(-5,65))
+
+# regression logx
+sns.set()
+sns.set_style('white')
+sns.set_context('paper')
+sns.set_palette('gray')
+fig, ax = plt.subplots()
+sreg1 = sns.regplot(x="pld", y="PCnum", data=df_indices, logx=True, x_jitter=0.1)
+ax.set(xlim=(-5,65))
+ax.set_xlabel('PLD (days)')
+#fig.savefig(r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Hakai\publications_figures\chap1\fig04_PCnum.svg')
+# How to explain in paper:
+# estimate a linear regression of the form y ~ log(x), but plot the scatterplot and regression model in the input space
+# no longer using that one
+# there's nothing wrong with this one, it's just that:
+# linearly doesn't make sense, even with logx. It's not a good fit and i know there is a curve.
+# However with order=2, it curves down at the end, which even though the data does do that, I know that it is not the relationship I am looking for.
+# It's most likely a logarithmic relationship, even if my data doesn't show that.
+# Therefore, I would need to model something with an asymptote.
+# Also, if you check the residuals, even with order=2, it is still not a good fit. This indicates there is some other relationship going on and I would need to model it differently.
+
+sns.residplot(x="pld", y="PCnum", data=df_indices, order=2)
+
+# log pld
+df_indlog = df_indices
+df_indlog['pldlog'] = np.log(df_indices.pld)
+sreg1 = sns.regplot(x="pldlog", y="PCnum", data=df_indlog, lowess=True, x_jitter=0.02)
+# lowess does local polynomial thing where it just looks at a local neighborhood. This is good for fitting a line to noisy data where you may not be able to detect the relationship.
+# However, I think I will have a hard time defending this.
+
+# do 1/x (this is similar to what would happen if there was an asymptote)
+df_ind1 = df_indices
+df_ind1['pld1'] = 1 / df_indices.pld
+sreg1 = sns.regplot(x="pld1", y="PCnum", data=df_ind1, order=1, x_jitter=0.01)
+sns.residplot(x="pld1", y="PCnum", data=df_ind1, order=1)
+# this has a good fit, indicating to me that I need to figure out how to model a curve that reaches an asymptote
+
+# try to fit my own curve
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import pandas as pd
+def f(x, a, b, n):
+    return a * x ** n  / (x ** n + b)
+y= df_indices.PCnum
+x= df_indices.pld 
+popt, pcov = curve_fit(f, x, y, p0=[445., 441., 2.])
+plt.scatter(x, y)
+plt.plot(x, f(x, *popt), 'r-')
+plt.show()
+
+# GLM
+# https://towardsdatascience.com/generalized-linear-models-9cbf848bb8ab
+# This still gave me a straight line
+import numpy as np
+from numpy.random import uniform, normal, poisson, binomial
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+x = df_indlog.pldlog.values
+y = df_indlog.PCnum.values
+exog, endog = sm.add_constant(x), y
+# Poisson regression
+mod = sm.GLM(endog, exog, family=sm.families.Poisson(link=sm.families.links.log()))
+res = mod.fit()
+display(res.summary())
+y_pred = res.predict(exog)
+idx = x.argsort()
+x_ord, y_pred_ord = x[idx], y_pred[idx]
+plt.plot(x_ord, y_pred_ord, color='m')
+plt.scatter(x, y,  s=20, alpha=0.8)
+plt.xlabel("X")
+plt.ylabel("Y")
